baa430acb23cbd70f670b61ac3400f78e34b14d4
DevLaukey <devlaukey@gmail.com>
2025-02-09 14:48:55 +0300

    Checking the keys

--- .next/static/chunks/app_layout_tsx_61af54._.js
+++ .next/static/chunks/app_layout_tsx_61af54._.js
@@ -6,7 +6,9 @@
   "path": "static/chunks/app_layout_tsx_61af54._.js",
   "chunks": [
     "static/chunks/[root of the server]__9d1267._.css",
-    "static/chunks/app_layout_tsx_83fc40._.js"
+    "static/chunks/node_modules_26669b._.js",
+    "static/chunks/app_layout_tsx_83fc40._.js",
+    "static/chunks/node_modules_@clerk_nextjs_dist_esm_app-router_a45e37._.js"
   ],
   "source": "dynamic"
 });
